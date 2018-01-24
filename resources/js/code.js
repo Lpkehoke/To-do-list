dec0ee88a74af961488a2f0aba1c4e86e354582e
alex <ibymif2000@yandex.ru>
2018-01-24 22:24:20 +0300

    all 0.01

--- resources/js/code.js
+++ resources/js/code.js
@@ -1,15 +1,89 @@
 'use strict';
 window.onload = function () {
-	let allBlockNotes = [];
+	let wrapper = document.querySelector('.wrapper');
 
-	function Note (str) {
-		let context = this;
+	document.querySelector('.btn-for-new-column').onclick = function () {
+		let column = new Column (document.querySelector('.title-new-column').value);
+		document.querySelector('.wrapper').insertBefore(column.obj , document.querySelector('.div-for-new-column'));
+			column.obj.querySelector('.form__btn').onclick = function create () {
+			let content = column.obj.querySelector('.textarea'),
+				obj = new Note (content.value , column.obj);
+			content.value = '';
+		};
+		document.querySelector('.title-new-column').value = '';
+	};
+
+
+	function Column (title) {
+		if (!title) throw 'Enter title of column';
+		let contextCol = this;
+
+		contextCol.title = title;
+
+		let removeBtn = document.createElement('button'),
+			editBtn = document.createElement('button');
+
+		removeBtn.className = 'remove-note';
+		editBtn.className = 'edit-note';
+
+
+		let divNotes = document.createElement('div'),
+			form = document.createElement('div'),
+			textareaCol = document.createElement('textarea'),
+			button = document.createElement('button'),
+			mainColumn = document.createElement('div'),
+			hTitle = document.createElement('h3');
+
+		hTitle.innerHTML = title;
+
+		divNotes.className = 'notes';
+		form.className = 'form';
+		textareaCol.className = 'form__textarea textarea';
+		button.className = 'form__btn';
+		mainColumn.className = 'column-note';
+
+		button.innerHTML = 'Create';
+
+		editBtn.onclick = function () {
+			let textarea = document.querySelector('.edit-textarea'),
+				bg = document.querySelector('.pop-up-meny');
+
+			textarea.value = contextCol.title;
+			bg.style.display = 'block';
+			document.querySelector('.save').onclick = function () {
+				return function () {
+					hTitle.innerHTML = textarea.value;
+					contextCol.title = textarea.value;
+					bg.style.display = 'none';
+				};
+			}();
+			document.querySelector('.pop-up__bg').onclick = function () {
+				bg.style.display = 'none';
+			};
+		};
+
+		removeBtn.onclick = function () {
+			mainColumn.remove();
+		};
+
+		form.appendChild(textareaCol);
+		form.appendChild(button);
+		mainColumn.appendChild(hTitle);
+		mainColumn.appendChild(editBtn);
+		mainColumn.appendChild(removeBtn);
+		mainColumn.appendChild(divNotes);
+		mainColumn.appendChild(form);
+		contextCol.obj = mainColumn;
+	};
+
+	function Note (str , parent) {
 		if (!str) throw 'There is not content';
-		context.content = str;
-		let div = document.createElement('div'),
+		let context = this,
+			div = document.createElement('div'),
 			divContent = document.createElement('div'),
 			removeBtn = document.createElement('button'),
 			editBtn = document.createElement('button');
+		context.content = str;
 
 		removeBtn.className = 'remove-note';
 		divContent.className = 'notes__note';
@@ -20,7 +94,7 @@ window.onload = function () {
 		div.appendChild(removeBtn);
 		div.appendChild(editBtn);
 		div.appendChild(divContent);
-		document.querySelector('.notes').appendChild(div);
+		parent.querySelector('.notes').appendChild(div);
 
 		removeBtn.onclick = function () {
 			div.remove();
@@ -29,24 +103,19 @@ window.onload = function () {
 		editBtn.onclick = function () {
 			let textarea = document.querySelector('.edit-textarea'),
 				bg = document.querySelector('.pop-up-meny');
+
 			textarea.value = context.content;
 			bg.style.display = 'block';
 			document.querySelector('.save').onclick = function () {
-				return function (){
+				return function () {
 					divContent.innerHTML = textarea.value;
 					context.content = textarea.value;
 					bg.style.display = 'none';
 				};
-			}();
+		}();
 			document.querySelector('.pop-up__bg').onclick = function () {
 				bg.style.display = 'none';
 			};
 		};
-	};
-
-	document.querySelector('.form__btn').onclick = function create () {
-		let content = document.querySelector('.textarea'),
-			obj = new Note (content.value);
-		content.value = '';
 	};
 };
